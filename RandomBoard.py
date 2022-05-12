3b883f35de32ed21c6d30f56645215ace3c36043
Cypress Kouri <72818091+CypressKouri@users.noreply.github.com>
2022-05-12 10:34:17 -0700

    commit

--- RandomBoard.py
+++ RandomBoard.py
@@ -1,6 +1,11 @@
 from random import randint
 
+sum = 0
+
 for i in range(10):
     for j in range (10): 
-        print (randint(0, 9), end = ' ')
+        rand = randint(0, 9)
+        sum += rand
+        print (rand, end = ' ')
     print("\n")
+print(sum)
